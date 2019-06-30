7e28466991a8be62b33d3edb0ac3683a80735aec
G6B-FkMz <52376198+G6B-FkMz@users.noreply.github.com>
2019-06-30 19:19:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require( 'discord.js') ;
 const client = new Discord.Client( ) ;
-
+const token = process.env.token
 var prefix = 't.'
 var token = 'NTk0OTA4NTY3ODI0MjM2NTY0.XRjbCg.rlM7B3ZX5XYr4drXU9Pm8GawzTs
 
